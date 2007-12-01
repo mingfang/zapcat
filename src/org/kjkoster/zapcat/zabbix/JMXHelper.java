a1d9269149b4a97a0fe7622dbe3088a1c789681d
kjkoster <kjkoster@5267cd63-cf3d-0410-a073-b21f34830dcd>
2007-12-01 13:21:06 +0000

    Updated the license notices.
    git-svn-id: https://zapcat.svn.sourceforge.net/svnroot/zapcat/trunk@27 5267cd63-cf3d-0410-a073-b21f34830dcd

--- src/org/kjkoster/zapcat/zabbix/JMXHelper.java
+++ src/org/kjkoster/zapcat/zabbix/JMXHelper.java
@@ -5,10 +5,10 @@ package org.kjkoster.zapcat.zabbix;
  * Zapcat is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
+ * (at your option) any later version.
  * 
  * Zapcat is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  * 
